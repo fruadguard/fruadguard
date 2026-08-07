21032fd32a43412bccb15871d2c9de7c53b23f27
fruadguard <rbiplob905@gmail.com>
2026-08-07 22:38:29 +0600

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,94 +1,108 @@
-import { auth } from "./firebase.js";
+import { auth, db } from "./firebase.js";
+
 
 import {
+
 createUserWithEmailAndPassword,
+
 signInWithEmailAndPassword,
-signOut
+
+GoogleAuthProvider,
+
+FacebookAuthProvider,
+
+signInWithPopup
+
 } from 
 "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 
-// Register User
+import {
+
+doc,
+
+setDoc
+
+} from 
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
+
+
+
+// Create Account
 
 window.registerUser = async function(){
+
+
+const name = document.getElementById("name").value;
 
 const email = document.getElementById("email").value;
 
 const password = document.getElementById("password").value;
 
+const confirmPassword = document.getElementById("confirmPassword").value;
+
+
+
+if(password !== confirmPassword){
+
+alert("Password does not match");
+
+return;
+
+}
+
+
 
 try{
 
-await createUserWithEmailAndPassword(
+
+const userCredential = await createUserWithEmailAndPassword(
+
 auth,
+
 email,
+
 password
+
 );
+
+
+
+const user = userCredential.user;
+
+
+
+await setDoc(doc(db,"users",user.uid),{
+
+
+name:name,
+
+email:email,
+
+createdAt:new Date()
+
+
+});
+
 
 
 alert("Account Created Successfully!");
 
-window.location.href="dashboard.html";
-
-
-}
-
-catch(error){
-
-alert(error.message);
-
-}
-
-};
-
-
-
-
-// Login User
-
-window.loginUser = async function(){
-
-const email = document.getElementById("email").value;
-
-const password = document.getElementById("password").value;
-
-
-try{
-
-await signInWithEmailAndPassword(
-auth,
-email,
-password
-);
-
-
-alert("Login Successful!");
 
 window.location.href="dashboard.html";
 
 
 }
 
+
+
 catch(error){
 
 alert(error.message);
 
 }
-
-};
-
-
-
-
-// Logout
-
-window.logoutUser = async function(){
-
-
-await signOut(auth);
-
-
-window.location.href="login.html";
 
 
 };
