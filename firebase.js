3aa24490f203e2413456b9d355651af1253cfc54
fruadguard <rbiplob905@gmail.com>
2026-08-07 22:17:42 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,14 +2,17 @@ import { initializeApp } from
 "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 
-import { 
-getAuth 
-} from 
+import { getAuth } from 
 "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
+import { getFirestore } from 
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 
 
 const firebaseConfig = {
+
   apiKey: "AIzaSyByZP0D_jKYOitPhOunqn44Z1FMNFF40tc",
   authDomain: "fraudguard-eafd2.firebaseapp.com",
   databaseURL: "https://fraudguard-eafd2-default-rtdb.firebaseio.com",
@@ -18,6 +21,7 @@ const firebaseConfig = {
   messagingSenderId: "721145048132",
   appId: "1:721145048132:web:5b40a07526b47230806011",
   measurementId: "G-M32E57QXD1"
+
 };
 
 
@@ -25,4 +29,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 
+
 export const auth = getAuth(app);
+
+
+export const db = getFirestore(app);
